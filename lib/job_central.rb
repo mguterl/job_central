51ae3c74fd3483769413a444ac2429ee0a28a37c
Michael Guterl <mguterl@gmail.com>
2009-04-12 19:52:25 -0400

    basic implementation

---
+++ lib/job_central.rb
@@ -0,0 +1,65 @@
+require 'rubygems'
+require 'time'
+require 'open-uri'
+require 'nokogiri'
+
+class JobCentral
+  BASE_URI = "http://xmlfeed.jobcentral.com"
+  
+  class Employer < Struct.new(:name, :file_uri, :file_size, :date_updated, :jobs)
+    def self.all
+      html = Nokogiri::HTML read
+      @employers = []
+      ((html/"table")[-1]/"tr").each_with_index do |element, idx|
+        next unless idx >= 2
+        attributes = element/"td"
+        
+        employer = Employer.new
+        employer.name = attributes[0].text
+        employer.file_uri = BASE_URI + (attributes[1]/"a").attr('href')
+        employer.file_size = attributes[2].text
+        employer.date_updated = Time.parse attributes[3].text
+
+        @employers << employer
+      end
+      @employers
+    end
+
+    def self.read(uri = BASE_URI + "/index.asp")
+      open(uri).read
+    end
+    
+    def read_jobs
+      open(file_uri).read
+    end
+    
+    def jobs
+      xml = Nokogiri::XML read_jobs
+      @jobs = []
+      
+      (xml/"job").each do |element|
+        job = Job.new
+        job.guid = element.at("guid").text
+        job.title = element.at("title").text
+        job.description = element.at("description").text
+        job.link = element.at("link").text
+        job.imagelink = element.at("imagelink").text
+        element.css("industry").each do |industry|
+          job.industries << industry.text
+        end
+        job.expiration_date = Date.parse(element.at("expiration_date").text)
+        job.employer_name = element.at("employer").text
+        job.location = element.at("location").text
+        @jobs << job
+      end
+
+      @jobs
+    end
+  end
+
+  class Job < Struct.new(:guid, :title, :description, :link, :imagelink, :industries, :expiration_date, :employer_name, :location)
+    def industries
+      @industries ||= []
+    end
+  end
+end
--- spec/job_central_spec.rb
+++ spec/job_central_spec.rb
@@ -1,7 +1,95 @@
-require 'spec_helper'
+require File.dirname(__FILE__) + '/spec_helper'
 
-describe "JobCentral" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe JobCentral do
+  before(:each) do
+    JobCentral::Employer.stub!(:read).
+      and_return(File.read(File.dirname(__FILE__) + "/fixtures/employers.html"))
+    @employers = JobCentral::Employer.all
+  end
+  
+  it "should fetch the list of employers" do
+    @employers.should respond_to(:each)
+  end
+
+  describe JobCentral::Employer do
+    before(:each) do
+      @media = @employers.first
+      @media.stub!(:read_jobs).
+        and_return(File.read(File.dirname(__FILE__) + "/fixtures/jobs.xml"))
+    end
+    
+    it "should have attributes parsed from the html" do
+      @media.should_not be_nil
+      @media.name.should == "1105 Media, Inc."
+      @media.file_uri.should == "#{JobCentral::BASE_URI}/feeds/1105media.xml"
+      @media.file_size.should == "15 KB"
+      @media.date_updated.should == Time.mktime(2009, 4, 12, 5, 59, 01)
+      @media.jobs.should respond_to(:each)
+    end
+
+    it "should read the html from job central" do
+      JobCentral::Employer.read.should == File.read(File.dirname(__FILE__) + "/fixtures/employers.html")
+    end
+
+    it "should read the xml feed of jobs" do
+      @media.read_jobs.should == File.read(File.dirname(__FILE__) + "/fixtures/jobs.xml")
+    end
+
+    describe JobCentral::Job do
+      before(:each) do
+        @writer = @media.jobs.first
+      end
+      
+      it "should have attributes parsed from the xml" do
+        @writer.guid.should == "1105media-24064"
+        @writer.title.should == "New Products Writer"
+        @writer.description.should == JOB_DESCRIPTION # check eof
+        @writer.link.should == "http://jcnlx.com/3eca112f27834df8b7dbd803d6ecf097105"
+        @writer.imagelink.should == "http://images.jobcentral.com/companylogos/1105media.gif"
+        @writer.industries.should == ["Media / Publishing"]
+        @writer.expiration_date.should == Date.new(2009, 4, 16)
+        @writer.employer_name.should == "1105 Media, Inc."
+        @writer.location.should == "Dallas, TX, 75219, USA"
+      end
+    end
   end
 end
+
+BEGIN {
+  JOB_DESCRIPTION = <<-EOF
+Job Details
+New Products Writer
+Send Job to Friend &amp;#187;
+Apply Now &amp;#187;
+Location: Dallas, TX
+Department: Editorial
+Job Type: Hire
+Openings: 0 open, out of 1 available
+
+* 
+1105 Media is looking for a detail-oriented New Products Writer in its Dallas, Texas office who can write and edit new products for several B2B magazines.
+Position Profile:
+
+As a New Products Writer, you will be responsible for writing 120 to 200 products per month. This requires excellent copyediting and organizational skills and the ability to describe the features and benefits of a product in 80 words or fewer. Occasionally you will be needed to proof articles and pages.
+
+Responsibilities include:
+* Writing 120 to 200 products per month.
+* Copyediting stories and page proofs.
+* Helping the editorial team post products to Web sites.
+* Sending out monthly e-mail blasts for new products.
+* Producing e-newsletters for magazines.
+
+The ideal candidate for this position will possess the following qualifications:
+* At least one year professional experience, preferably in communications or journalism.
+* BA/BS in Journalism/English/Communications or related field of study.
+Experience in the following areas will be considered a plus:
+* HMTL coding
+* B2B publishing
+* Sitecore CMS
+1105 Media is based in Chatsworth,CA, with primary offices throughtout the United States. The company was formed in April 2006 by Nautic Partners LLC, Alta Communications, and President/CEO Neal Vitale.
+We offer a competitive salary and a comprehensive benefits package that includes medical/dental/vision insurance, life insurance, disability insurance, 401(k) plan, and a generous paid time off (PTO)/holiday plan.
+We are an equal opportunity employer.
+Send Job to Friend &amp;#187;
+Apply Now &amp;#187;
+EOF
+}
