5b739f3ec0fcb8ba870b07ba947ac7c0d76096a2
Michael Guterl <mguterl@gmail.com>
2009-04-12 19:58:38 -0400

    simple refactorings

--- lib/job_central.rb
+++ lib/job_central.rb
@@ -8,7 +8,6 @@ class JobCentral
   
   class Employer < Struct.new(:name, :file_uri, :file_size, :date_updated, :jobs)
     def self.all
-      html = Nokogiri::HTML read
       @employers = []
       ((html/"table")[-1]/"tr").each_with_index do |element, idx|
         next unless idx >= 2
@@ -28,13 +27,20 @@ class JobCentral
     def self.read(uri = BASE_URI + "/index.asp")
       open(uri).read
     end
+
+    def self.html
+      Nokogiri::HTML read
+    end
     
     def read_jobs
-      open(file_uri).read
+      read file_uri
+    end
+
+    def xml
+      Nokogiri::XML read_jobs
     end
     
     def jobs
-      xml = Nokogiri::XML read_jobs
       @jobs = []
       
       (xml/"job").each do |element|
@@ -44,12 +50,12 @@ class JobCentral
         job.description = element.at("description").text
         job.link = element.at("link").text
         job.imagelink = element.at("imagelink").text
-        element.css("industry").each do |industry|
-          job.industries << industry.text
-        end
         job.expiration_date = Date.parse(element.at("expiration_date").text)
         job.employer_name = element.at("employer").text
         job.location = element.at("location").text
+        element.css("industry").each do |industry|
+          job.industries << industry.text
+        end
         @jobs << job
       end
 
